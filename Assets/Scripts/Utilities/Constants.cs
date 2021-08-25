ac90d819d56dd45b5c952cdd2e89c6eb0be3cd19
RobertJClose <RobertClose+Programming@ProtonMail.com>
2021-08-25 11:48:19 +0100

    Increased max Lambert solver Householder iterations

--- Assets/Scripts/Utilities/Constants.cs
+++ Assets/Scripts/Utilities/Constants.cs
@@ -7,7 +7,7 @@ public static class Constants
     public static readonly int KeplerEquationMaxIterations = 100; 
     public static readonly double KeplerEquationPrecision = 1e-15;
 
-    public static readonly int LambertSolverMaxIterations = 5;
+    public static readonly int LambertSolverMaxIterations = 10;
 
     public static readonly float PlotRescaleFactor = 1.0f / 600_000.0f;
     public static readonly float OrbitPlotThickness = 1.0f;
