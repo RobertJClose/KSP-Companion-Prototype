c09df076e0968346e0cf3d99de5493413f4e50fd
RobertJClose <RobertClose+Programming@ProtonMail.com>
2021-10-23 17:43:24 +0100

    Correct value for Anglef.MaxAngle in Anglef.cs

--- Assets/KSP Companion/Scripts/Utilities/Anglef.cs
+++ Assets/KSP Companion/Scripts/Utilities/Anglef.cs
@@ -53,7 +53,7 @@ public readonly struct Anglef : System.IEquatable<Anglef>, System.IComparable<An
     /// <summary>
     /// Gets an angle with the largest possible value for an angle before wrapping around 2PI.
     /// </summary>
-    public static Anglef MaxAngle => new Anglef(6.28318500519f);
+    public static Anglef MaxAngle => new Anglef(6.28318500518798828125f);
     
     /// <summary>
     /// Gets an angle of one quarter turn.
